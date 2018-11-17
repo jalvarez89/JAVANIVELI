419a01b3504360341328f138e3a916b91cc44b82
Jesus Armando Alvarez <agromotrizja@gmail.com>
2018-11-17 18:25:06 -0400

    Fin 1era Clase

---
+++ Clase 1/Clase1_1/src/MainClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Esta Clase es de Ejemplo
+ * @author Jesus Alvarez
+ */
+public class MainClass {
+    
+    public static void main(String[] args){
+    //Este Programa es para un Nombre
+        System.out.print("Nombre: ");
+        System.out.println("Jesus Alvarez");
+    }
+}
